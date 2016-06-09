370d9a860af06d68a309620c551ca4804ebc6eff
pratirvce <pratirvce@gmail.com>
2016-06-09 14:47:58 -0700

    Pattern Matching

--- patternmatch.py
+++ patternmatch.py
@@ -1,4 +1,20 @@
+def search(input_pattern,input_text):
+    M = len(input_pattern)
+    N = len(input_text)
+
+    for i in range(N - M + 1):
+        for j in range(M):
+            if input_text[i+j] != input_pattern[j]:
+                break
+
+        if j == M-1:
+            print("Pattern found at index "+str(i))
 
 
+print ("Enter the text: ")
+input_text=input()
+print("Enter the pattern: ")
+input_pattern=input()
+search(input_pattern,input_text)
 
-print ("Hello World")
+
